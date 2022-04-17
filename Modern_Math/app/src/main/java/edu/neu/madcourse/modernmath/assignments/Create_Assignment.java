b69670322740d2b1f1c5952d896dcf1f72de3f3e
beelerb <[beeler.b@husky.neu.edu]>
2022-04-17 15:56:27 -0400

    Create activity for creating assignments

--- Modern_Math/app/src/main/java/edu/neu/madcourse/modernmath/assignments/Create_Assignment.java
+++ Modern_Math/app/src/main/java/edu/neu/madcourse/modernmath/assignments/Create_Assignment.java
@@ -2,14 +2,18 @@ package edu.neu.madcourse.modernmath.assignments;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -33,9 +37,10 @@ public class Create_Assignment extends AppCompatActivity {
             actionBar.setIcon(R.mipmap.ic_launcher_mm_round);
         }
 
-        // Level Selection - Default selection as easy
+        // Difficulty Selection - Default selection as easy
         Spinner difficultySpinner = (Spinner) findViewById(R.id.difficulty_spinner);
-        difficultySpinner.setAdapter(new ArrayAdapter<Difficulty>(this, android.R.layout.simple_spinner_item, Difficulty.values()));
+        difficultySpinner.setAdapter(new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, Difficulty.values()));
 
 
         difficultySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -47,16 +52,57 @@ public class Create_Assignment extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // TODO Auto-generated method stub
+
             }
         });
 
+        NumberPicker time_picker = findViewById(R.id.time_picker);
+        time_picker.setMinValue(0);
+        time_picker.setMaxValue(12);
+
+        String[] timeValues = getResources().getStringArray(R.array.time_array);
+
+        time_picker.setDisplayedValues(timeValues);
+
+        NumberPicker num_questions_picker = findViewById(R.id.num_questions_picker);
+        num_questions_picker.setMinValue(0);
+        num_questions_picker.setMaxValue(20);
+
+        String[] numQuestionValues = getResources().getStringArray(R.array.num_q_array);
+        num_questions_picker.setDisplayedValues(numQuestionValues);
 
 
-    }
+        SwitchCompat add_switch = findViewById(R.id.addition_switch);
+        SwitchCompat sub_switch = findViewById(R.id.subtraction_switch);
+        SwitchCompat multi_switch = findViewById(R.id.multiplication_switch);
+        SwitchCompat div_switch = findViewById(R.id.division_switch);
 
-    public void createNewAssignment(View view)
-    {
+        Button create_button = findViewById(R.id.create_assignment_button);
+        create_button.setOnClickListener(view -> {
+            if (add_switch.isChecked() || sub_switch.isChecked() ||
+                    multi_switch.isChecked() || div_switch.isChecked())
+            {
+                if (timeValues[time_picker.getValue()].equals("Timer Off")
+                        && numQuestionValues[num_questions_picker.getValue()].equals("No target"))
+                {
+                    Toast.makeText(Create_Assignment.this, "Turn on either the timer " +
+                                    "or set a target number of questions to be answered.",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    // TODO: create assignment and push to firebase. Will retrieve on previous screen
+                    //  Will need to have info on which teacher is making the assignment/what class
+                    Toast.makeText(Create_Assignment.this, "TODO- create assignment",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+            else
+            {
+                Toast.makeText(Create_Assignment.this, "At least one operator " +
+                        "must be selected.", Toast.LENGTH_SHORT).show();
+            }
 
+        });
     }
 }
