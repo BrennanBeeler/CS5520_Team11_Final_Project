954ff507bacc629af21aa5f2c08696a478e8959d
Nidhi Shah <nidhi@Nidhis-MacBook-Pro.local>
2022-04-25 15:17:37 -0400

    Adding score and class_code

--- Modern_Math/app/src/main/java/edu/neu/madcourse/modernmath/database/User.java
+++ Modern_Math/app/src/main/java/edu/neu/madcourse/modernmath/database/User.java
@@ -32,10 +32,8 @@ public class User implements Parcelable {
     @ColumnInfo(name = "is_teacher")
     public boolean is_teacher;
 
-    @ColumnInfo(name = "class_code")
     public String class_code;
 
-    @ColumnInfo(name = "answers")
     public int answers;
 
 
@@ -63,6 +61,8 @@ public class User implements Parcelable {
         this.age = userLoginCard.age;
         this.active = userLoginCard.active;
         this.is_teacher = userLoginCard.is_teacher;
+        this.class_code = userLoginCard.class_code;
+        this.answers = userLoginCard.answers;
     }
 
     public int describeContents() {
@@ -77,6 +77,8 @@ public class User implements Parcelable {
         // Cannot use writeBoolean with API level of 26
         out.writeInt(this.active ? 1 : 0);
         out.writeInt(this.is_teacher ? 1 : 0);
+        out.writeString(this.class_code);
+        out.writeInt(this.answers);
     }
 
     public static final Parcelable.Creator<User> CREATOR
@@ -98,6 +100,8 @@ public class User implements Parcelable {
         // Cannot use readBoolean with API level of 26
         this.active = in.readInt() == 1;
         this.is_teacher = in.readInt() == 1;
+        this.class_code = in.readString();
+        this.answers = in.readInt();
     }
 
     @Override
